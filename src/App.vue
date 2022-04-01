b2a27d0124c8ee007528cfdd9203b6ddeefde6ae
AZ <agnieszkayd@gmail.com>
2022-04-01 17:00:14 +0200

    added axios to the project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import axios from 'axios';
+export default {
+  name: 'App',
+  methods: {
+    getFoxImage(){
+     axios.get('https://some-random-api.ml/docs/endpoints/animal#fox')
+    }
+  }
+}
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
-
+  <image/>
   <RouterView />
 </template>
 
